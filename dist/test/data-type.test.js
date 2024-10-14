c14df802a4031e57263a3e5051d1cc3147ec841c
rizki <muhammadrizki0035@gmail.com>
2024-10-14 13:08:30 +0700

    test commit

---
+++ dist/test/data-type.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe("Data Type", () => {
+    it("shuld describe", () => {
+        const nama = "Mochammad Rizki";
+        const balance = 100000000;
+        const isVip = true;
+        console.log(nama);
+        console.log(balance);
+        console.log(isVip);
+    });
+});
---
+++ test/data-type.test.ts
@@ -0,0 +1,11 @@
+describe("Data Type", () => {
+    it("shuld describe", () => {
+        const nama: string = "Mochammad Rizki"
+        const balance: number = 100000000
+        const isVip: boolean = true
+
+        console.log(nama)
+        console.log(balance)
+        console.log(isVip)
+    })
+})
